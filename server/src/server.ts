8882b68e39a31b2f8576b8aae0e1904445455c5e
DanieldelimaS <danielsuporteti@outlook.com.br>
2023-06-01 21:24:23 -0300

    conceitos basicos de rotas, requisiçoes, response and json

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,34 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
+app.use(express.json());
+
+
+const users =[
+    'Daniel',
+    'Diego',
+    'Pedro',
+    'Joao'
+];
 
 app.get('/users',(request, response) => {
-    console.log('Listagem de usuários');
+   const search = String(request.query.search);
 
+   const filteredUsers = search?users.filter(user => user.includes(search)):users;
     //json
-    response.json([
-        'Daniel',
-        'Diego',
-        'Pedro',
-        'Joao'
-    ]);
+    return response.json(filteredUsers);
 });
+
+app.get('/users/:id',(request,response)=>{
+const id = Number (request.params.id);
+const user = users[id]; 
+return response.json(user);
+});
+
+app.post('/users',(request,response)=>{
+    const data = request.body;
+
+    return response.json(data);
+})
 
 app.listen(3333);
